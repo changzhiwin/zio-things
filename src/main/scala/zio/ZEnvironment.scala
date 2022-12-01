573f1631194cee26f591e634631c52ebe59ae29a
changzhiwin <changzhiwin@gmail.com>
2022-12-01 22:56:02 +0800

    ZEnvironment demo

---
+++ src/main/scala/zio/ZEnvironment.scala
@@ -0,0 +1,30 @@
+package zio
+
+import scala.reflect.{ClassTag}
+
+trait ZEnvironment[+R] {
+
+  def add[A](a: A)(implicit atag: ClassTag[A]): ZEnvironment[R with A]
+
+  def get[A >: R](implicit atag: ClassTag[A]): A
+
+}
+
+// what is the actually type of the K part of Map[ClassTag[_], Any]?
+// val int = implicitly[ClassTag[String]]
+// val str = implicitly[ClassTag[Int]]
+// val list = implicitly[ClassTag[List[Int]]]
+
+// Set(int):            Set[scala.reflect.ClassTag[String]]
+// Set(int, str):       Set[scala.reflect.ClassTag[_ >: Int with String]]
+// Set(int, str, list): Set[scala.reflect.ClassTag[_ >: List[Int] with Int with String]] 
+class ZEnvironmentLive[+R](typeMap: Map[ClassTag[_], Any]) extends ZEnvironment[R] {
+
+  def add[A](a: A)(implicit atag: ClassTag[A]): ZEnvironment[R with A] = {
+    new ZEnvironmentLive[R with A](typeMap ++ Map(atag -> a) )
+  }
+
+  def get[A >: R](implicit atag: ClassTag[A]): A = {
+    typeMap(atag).asInstanceOf[A]
+  }
+}
